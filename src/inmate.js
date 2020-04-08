7cfcce629df3cc6153e0f0cc94f42bad6e84edc9
jandersen770 <jandersen770@gmail.com>
2020-04-07 21:38:17 -0400

    Update

--- src/inmate.js
+++ src/inmate.js
@@ -4,8 +4,8 @@ class Inmate {
         this.x = x;
         this.y = y;
 
-        this.dx = Math.random() * 1.5;
-        this.dy = Math.random() * 1.5;
+        this.dx = Math.random() * 1.2 * (Math.random() >= .5 ? 1 : -1);
+        this.dy = Math.random() * 1.2 * (Math.random() >= .5 ? 1 : -1);
 
         this.mass = this.radius * this.radius * this.radius;
         this.color = ["orange", "brown", "red", "yellow"][Math.floor(Math.random() * 4)];
@@ -13,21 +13,11 @@ class Inmate {
         this.img = new Image();
         this.img.src = "https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png";
         
-        this.currentDirection = 0;
+        this.currentDirection = Math.abs(this.dx) > Math.abs(this.dy) ? this.dx > 0 ? 3 : 2 : this.dy > 0 ? 0 : 1;
         this.currentLoopIndex = 0;
 
         this.levee = 0;
     };
-
-    // draw() {
-    //     ctx.beginPath();
-    //     ctx.arc(Math.round(this.x), Math.round(this.y), this.radius, 0, 2 * Math.PI);
-    //     ctx.fillStyle = this.color;
-    //     ctx.fill();
-    //     ctx.strokeStyle = 'rgba(0, 0, 0, 0.6)';
-    //     ctx.stroke();
-    //     ctx.closePath();
-    // };
 
     speed() {
         return Math.sqrt(this.dx * this.dx + this.dy * this.dy);
